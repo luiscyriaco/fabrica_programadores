368d79b9bc2430a5d1fe7827c0422e4775f5cdee
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-06-28 11:52:08 -0300

    Add files via upload

---
+++ 06-funcao.py
@@ -0,0 +1,15 @@
+def escrever():
+    print('Hello World!')
+    
+    
+def line():
+    print('-' * 20)
+    
+
+line()
+escrever()
+line()
+escrever()
+line()
+escrever()
+line()
---
+++ 06.1-funcao_parametro.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    print(msg)
+
+escreva('-' * 40)
+escreva('Fábrica de Programadores')
+escreva('-' * 40)
+escreva('Turma de Sábado')
+escreva('-' * 40)
+escreva('Luis Ricardo')
+escreva('-' * 40)
